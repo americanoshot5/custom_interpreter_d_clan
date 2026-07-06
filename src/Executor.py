dea4cf160a67f30ef1473dd64b72c10c24fe7fd1
s.yeou <kaljae710@gmail.com>
2026-07-06 16:15:13 +0900

    feature - executor - test_execute_single_literal - pass

--- src/Executor.py
+++ src/Executor.py
@@ -1,12 +1,24 @@
 from __future__ import annotations
-
-from common import ExecuteError, Program, RuntimeValue
-from interfaces import Executor
-
+from common import *
+from interfaces import *
 
 class SExpressionExecutor(Executor):
-    def execute(self, program: Program) -> RuntimeValue:
-        raise ExecuteError("Executor implementation is not ready yet.")
+    def execute(self, program: Program):
+        result = None
+        for stmt in program.statements:
+            result = self._execute_stmt(stmt)
+        return result
+
+    def _execute_stmt(self, stmt):
+        if isinstance(stmt, ExpressionStmt):
+            return self._evaluate(stmt.expression)
+        raise ExecuteError("Unsupported statement")
+
+    def _evaluate(self, expr):
+        if isinstance(expr, LiteralExpr):
+            return expr.value
+
+        raise ExecuteError("Unsupported expression")
 
 
 DefaultExecutor = SExpressionExecutor
