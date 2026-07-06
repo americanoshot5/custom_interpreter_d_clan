a9c8b8c7cb6aadd0f410ff7c423300961f543e4c
ildo.kim <aknife@naver.com>
2026-07-07 08:53:14 +0900

    4 test case pass & refactoring

--- src/Tokenizer.py
+++ src/Tokenizer.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import Sequence
+from typing import Literal, Any
 
 from common import Token, TokenizeError, TokenType, SINGLE_CHAR_TOKENS, KEYWORDS, SourceLocation
 from interfaces import Tokenizer
@@ -19,29 +20,47 @@ class SExpressionTokenizer(Tokenizer):
 
         for t in token:
             if t.isdigit():
-                type = TokenType.NUMBER
-                literal = float(t)
+                self.set_digit_token(t)
             else:
-                if len(t) == 1:
-                    if t in SINGLE_CHAR_TOKENS.keys():
-                        type = SINGLE_CHAR_TOKENS[t]
-                        literal = t
-                    else:
-                        raise TokenizeError(f'{t} is not a valid literal')
-                elif t in KEYWORDS.keys():
-                    type = KEYWORDS[t]
-                    literal = t
-                    if type == TokenType.FALSE:
-                        literal = False
-                    if type == TokenType.TRUE:
-                        literal = True
-                else:
-                    type = TokenType.STRING
-                    literal = t.strip("\"")
-
-            self.token.append(Token(type=type, lexeme=t, literal=literal))
+                self.set_non_digit_token(t)
         self.token.append(Token(TokenType.EOF, ""))
         return self.token
+
+    def set_non_digit_token(self, t):
+        if len(t) == 1:
+            self.set_single_char_token(t)
+        elif t in KEYWORDS.keys():
+            self.set_keword_token(t)
+        else:
+            self.set_string_token(t)
+
+    def set_string_token(self, t):
+        type = TokenType.STRING
+        literal = t.strip("\"")
+        self.token.append(Token(type=type, lexeme=t, literal=literal))
+
+    def set_keword_token(self, t):
+        type = KEYWORDS[t]
+        literal = t
+        if type == TokenType.FALSE:
+            literal = False
+        if type == TokenType.TRUE:
+            literal = True
+        self.token.append(Token(type=type, lexeme=t, literal=literal))
+
+
+    def set_single_char_token(self, t):
+        if t in SINGLE_CHAR_TOKENS.keys():
+            type = SINGLE_CHAR_TOKENS[t]
+            literal = t
+        else:
+            raise TokenizeError(f'{t} is not a valid literal')
+        self.token.append(Token(type=type, lexeme=t, literal=literal))
+
+    def set_digit_token(self, t):
+        type = TokenType.NUMBER
+        literal = float(t)
+        self.token.append(Token(type=type, lexeme=t, literal=literal))
 
 
 DefaultTokenizer = SExpressionTokenizer
